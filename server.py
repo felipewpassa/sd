3e61591241e7f45e3bbfc71d956573e2c0535c95
Felipe Passa <felipewpassa@gmail.com>
2019-08-20 01:49:08 -0300

    Adicionado calculo e envio do time para cada slave

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ global host
 global port
 global s
 
-host = "127.0.0.1"
+host = "10.0.0.106"
 port = 9999
 s = socket.socket()
 
@@ -39,6 +39,29 @@ while True:
             dados = conn.recv(20480).decode()
             connIp, connPort = conn.getsockname()
             print(connIp + " - " + str(dados))
+    elif (comando == "sync"):
+        acum = 0
+        cont = 0
+        times = []
+        for idx, conn in enumerate(all_connections):
+            conn.send(comando.encode())
+            timeData = conn.recv(20480).decode()
+            connIp, connPort = conn.getsockname()
+            print(connIp + " - " + str(timeData) + '\n')
 
+            times.append({'ip': connIp, 'conn': conn, 'timestamp': timeData})
+
+            acum += float(timeData)
+            cont += 1
+        media = acum / cont
+
+        #print('acum: ' + str(acum) + ' / cont: ' + str(cont) + ' = media: ' + str(media))
+
+        for time in times: #Calcula a diferenca entre a media e o horario de cada maquina
+            #print("media " + str(media) + ' <- timestamp -> ' + str(time['timestamp']))
+            diferenca = float(media) - float(time['timestamp'])
+            print("Diferenca " + str(time['ip']) + ' -> ' + str(diferenca))
+
+            time['conn'].send(str(diferenca).encode())
 
 
